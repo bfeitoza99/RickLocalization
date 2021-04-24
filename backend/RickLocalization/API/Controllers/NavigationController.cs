33ce5c83f897a1a11bf68300c4835264dc48477d
BrunoFeitoza <bruno.feitoza10@hotmail.com>
2021-04-24 19:44:24 -0300

    adding mediatr and create navigation query architecture

---
+++ backend/RickLocalization/API/Controllers/NavigationController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RickLocalization.Application.Query.Navigation;
+
+namespace RickLocalization.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NavigationController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("")]
+        public IActionResult GetById(
+                                    [FromServices] IMediator mediator,
+                                    [FromQuery] NavigationQueryRequest command)
+        {
+            var result = mediator.Send(command);
+            return Ok(result);
+        }
+    }
+}
---
+++ backend/RickLocalization/Application/Query/Navigation/NavigationQueryHandle.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RickLocalization.Application.Query.Navigation
+{
+    class NavigationQueryHandle
+    {
+    }
+}
